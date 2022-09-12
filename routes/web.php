1ed03c919cfeb4312a0151e1257062e2b07d8381
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-09-12 22:07:17 +0600

    Dashboard Routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth','role:user'])->name('dashboard');
+
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::controller(DashboardController::class)->group(function(){
+        Route::get('/admin/dashboard','index');
+
+    });
+});
 
 require __DIR__.'/auth.php';
