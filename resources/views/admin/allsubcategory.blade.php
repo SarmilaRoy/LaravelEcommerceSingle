bc6be4434a72ee9c0fdf66fee867fae9ab4e2bcb
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-09-13 23:19:24 +0600

    All Sub Category Page Setup With Table

--- resources/views/admin/allsubcategory.blade.php
+++ resources/views/admin/allsubcategory.blade.php
@@ -1,7 +1,44 @@
 @extends('admin.layouts.template')
 @section('page_title')
-All Sub Category - Single Ecom 
+    All Sub Category - Single Ecom
 @endsection
 @section('content')
-<h2>Hi From allSubCategory</h2> 
-@endsection
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page/</span> All Sub Category</h4>
+        <!-- Bootstrap Table with Header - Light -->
+        <div class="card">
+            <h5 class="card-header">Available Sub Category Information</h5>
+            <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Id</th>
+                            <th>Sub Category Name</th>
+                            <th>Category</th>
+                            <th>Products</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <tr>
+                            <td>1</td>
+                            <td>Fan</td>
+                            <td>Electronices</td>
+                            <td>100</td>
+                            <td>
+                                {{-- <a href="" class="btn btn-sm btn-success">Edit</a>
+                                <a href="" onclick="return confirm('are you sure to delete?')"
+                                    class="btn btn-sm btn-danger">Delete</a> --}}
+                                    <a href="" class="btn btn-primary">Edit</a>
+                                <a href="" class="btn btn-warning">Delete</a>
+                                {{-- <a href="" class="btn btn-success"><i class="las la-edit"></i></a>
+                                <a href="" class="btn btn-danger"><i class="las la-times"></i></a> --}}
+                            </td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endsection
--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -18,6 +18,10 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+
+    
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
