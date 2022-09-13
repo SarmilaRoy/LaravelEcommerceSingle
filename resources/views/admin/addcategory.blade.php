76287f6ba4920d9a110027da213a8c89f8b72256
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-09-13 23:10:08 +0600

    Add Sub Category Page Setup With Form

--- resources/views/admin/addcategory.blade.php
+++ resources/views/admin/addcategory.blade.php
@@ -23,7 +23,7 @@
                                         placeholder="Electronices" />
                                 </div>
                             </div>
-                          
+
                             <div class="row justify-content-end">
                                 <div class="col-sm-10">
                                     <button type="submit" class="btn btn-primary">Add Category</button>
